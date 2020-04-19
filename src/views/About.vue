99c54edefb06e6a8334f9c64fae77f3e4d1869aa
pamelang <p.amelang@logic-joe.com>
2020-04-19 21:58:51 +0200

    Init

---
+++ src/views/About.vue
@@ -0,0 +1,9 @@
+<template>
+	<div class="about">
+		<p>
+			First little project using Vue :)
+			<br />This project is based on:
+			https://www.youtube.com/watch?v=Wy9q22isx3U
+		</p>
+	</div>
+</template>
